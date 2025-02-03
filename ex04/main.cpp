66c6de4ca7461a0162fbb3efd76512018b709de0
Gaelle Ozon <gozon@paul-f4Ar7s8.clusters.42paris.fr>
2025-02-03 10:30:11 +0100

    Better replace

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,34 +6,80 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 09:27:32 by gozon             #+#    #+#             */
-/*   Updated: 2025/02/03 09:03:08 by gozon            ###   ########.fr       */
+/*   Updated: 2025/02/03 10:25:31 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "replace.hpp"
+#include <string>
+#include <iostream>
+#include <fstream>
 
-int	main(int ac, char **av) {
+void    myReplace(std::string& string, std::string old_str, std::string new_str)
+{
 
-    std::string fstring = "";
-    std::string outfile;
+    size_t  pos = string.find(old_str, 0);
+    size_t  oldlen = old_str.length();
+    size_t  newlen = new_str.length();
 
+    while (pos != std::string::npos)
+    {
+        string.erase(pos, oldlen);
+        string.insert(pos, new_str);
+        pos += newlen;
+        pos = string.find(old_str, pos);
+    }
+
+}
+
+void    replaceStringFile(std::ifstream& ifs, std::ofstream& ofs, 
+            std::string old_str, std::string new_str) {
+    
+    
+    std::string line;
+    
+    while(std::getline(ifs, line)) {
+        myReplace(line, old_str, new_str);
+        ofs << line;
+        if (ofs.bad()) {
+            std::perror("ofs: error while writing to file");
+            break;
+        }
+        if (!ifs.eof()) {
+            ofs << "\n";
+        }
+    }
+        if (ifs.bad()) {
+            std::perror("ifs: error while reading file");
+        }
+                 
+}
+
+int main(int ac, char **av) {
+    
     if (ac != 4) {
         std::cout << "Usage ./replace filename s1 s2" << std::endl;
         return (0);
     }
 
-    if (!file_to_string(av[1], fstring))
-    {
+    std::ifstream ifs(av[1]);
+    if (!ifs.is_open()) {
+        std::perror("ifs: error while opening file");
         return (1);
     }
-    replace_all(fstring, av[2], av[3]);
-    outfile = (std::string)av[1] + ".replace";
-    if (!string_to_file(fstring, outfile.c_str()))
-    {
+    
+    std::string outfile = (std::string)av[1] + ".replace";
+    std::ofstream ofs(outfile.c_str(), std::fstream::out | std::fstream::trunc);
+    if (!ofs.is_open()) {
+        std::perror("ofs: error while opening file");
+        ifs.close();
         return (1);
     }
+
+    replaceStringFile(ifs, ofs, av[2], av[3]);
+
+    ifs.close();
+    ofs.close();
 
     return (0);
+    
 }
-
-int main(int ac, char **av)
