dfe7ec1b86b837afb265e0a09bd3f7135b71a55a
Gaelle Ozon <gozon@paul-f4Ar7s8.clusters.42paris.fr>
2025-02-03 10:34:26 +0100

    Better horde error handling

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 10:57:12 by gozon             #+#    #+#             */
-/*   Updated: 2025/01/26 11:09:52 by gozon            ###   ########.fr       */
+/*   Updated: 2025/02/03 10:32:54 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,16 @@ int main(int ac, char **av) {
     int                 N;
     Zombie              *horde;
 
-    if (ac != 3)
+    if (ac != 3) {
+        std::cout << "Usage ./horde number name\n";
         return (1);
+    }
     nstring << av[1];
     nstring >> N;
 
     horde = zombieHorde(N, av[2]);
+    if (!horde)
+        return (1);
     for (int i = 0; i < N; i++) {
         std::cout << "Zombie number " << i << ": ";
         horde[i].announce();
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 10:42:33 by gozon             #+#    #+#             */
-/*   Updated: 2025/01/26 10:55:29 by gozon            ###   ########.fr       */
+/*   Updated: 2025/02/03 09:05:52 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 Zombie  *zombieHorde(int N, std::string name) {
 
+    if (N < 1) {
+        std::cout << "N must be >= 1" << std::endl;
+        return (NULL);
+    }
+    
     Zombie  *horde = new Zombie[N];
-
     for (int i = 0; i < N; i++) {
         horde[i].setName(name);
     }
